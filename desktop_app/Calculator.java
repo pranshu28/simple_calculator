4948ea5974b562a7acf7c39a4c59b32334ee9ba5
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-07-10 09:48:37 +0530

    Create Calculator.java

---
+++ desktop_app/Calculator.java
@@ -0,0 +1,54 @@
+package func;
+
+public class Calculator {
+	public double currentVariable = 0;
+	
+	private void add(double secondOperand) {
+		this.currentVariable += secondOperand;
+	}
+	
+	private void subtract(double secondOperand) {
+		this.currentVariable -= secondOperand;
+	}
+	
+	private void multiply(double secondOperand) {
+		this.currentVariable *= secondOperand;
+	}
+	
+	private void divide(double secondOperand) {
+		this.currentVariable /= secondOperand;
+	}
+	
+	private void modulo(double secondOperand) {
+		this.currentVariable %= secondOperand;
+	}
+	
+	private void power(double secondOperand) {
+		this.currentVariable = (double)Math.pow(currentVariable, secondOperand);
+	}
+	
+	//Call Functions
+	public void useCalculator( char operator, double input) {
+		
+		switch (operator) {
+			case '+':
+				add(input);
+				break;
+			case '-':
+				subtract(input);
+				break;
+			case '*':
+				multiply(input);
+				break;
+			case '/':
+				divide(input);
+				break;
+			case '%':
+				modulo(input);
+				break;
+			case '^':
+				power(input);
+				break;
+		}
+	}
+}
